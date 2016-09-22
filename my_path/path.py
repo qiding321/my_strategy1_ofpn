f8419bb8cced6e09d14f1ed53468c0cc0c2b34c0
qiding <qiding321@126.com>
2016-09-22 18:50:28 +0800

    path dependent on the computer name

--- my_path/path.py
+++ my_path/path.py
@@ -5,13 +5,17 @@ Created on 2016/8/30 10:12
 @author: qiding
 """
 
+import socket
 
-# data_source_path = 'I:\\OrderFlowPredictData\\601818\\'
+name = socket.gethostname()
 
-# data_source_path = 'I:\\OrderFlowPredictData_from_raw_data\\601818\\'
-# market_making_result_root = 'E:\\StrategyResult\\MarketMaking\\'
-# log_path_root = market_making_result_root + 'Log\\'
-
-data_source_path = 'C:\\Users\\dqi\\Documents\\Data\\OrderFlowPredictData_from_raw_data\\601818\\'
-market_making_result_root = 'C:\\Users\\dqi\\Documents\\Output\\MarketMaking\\'
-log_path_root = market_making_result_root + 'Log\\'
+if name == '2013-20151201LG':
+    data_source_path = 'I:\\OrderFlowPredictData_from_raw_data\\601818\\'
+    market_making_result_root = 'E:\\StrategyResult\\MarketMaking\\'
+    log_path_root = market_making_result_root + 'Log\\'
+elif name == 'sas5':
+    data_source_path = 'C:\\Users\\dqi\\Documents\\Data\\OrderFlowPredictData_from_raw_data\\601818\\'
+    market_making_result_root = 'C:\\Users\\dqi\\Documents\\Output\\MarketMaking\\'
+    log_path_root = market_making_result_root + 'Log\\'
+else:
+    raise NameError

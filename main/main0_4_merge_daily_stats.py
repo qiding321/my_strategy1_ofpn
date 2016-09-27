3f6b691a7aa26bff4764c773dd497178c8d80971
qiding <qiding321@126.com>
2016-09-27 14:46:28 +0800

    add merge daily stats

---
+++ main/main0_4_merge_daily_stats.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2016/9/27 14:12
+
+@author: qiding
+"""
+
+import os
+
+import pandas as pd
+
+import my_path.path
+
+path_root = my_path.path.market_making_result_root
+fold_name = '2016-09-27-12-06-12rolling_error_decomposition_12M_predict_1M_normalized_by_12M_selected_vars'
+
+dates = [x for x in os.listdir(path_root + fold_name) if len(x.split('.')) == 1]
+predict_date = [x[18:18 + 8] for x in dates]
+
+
+def _str2datestr_(date_tuple_str):
+    t = date_tuple_str.replace('(', '').replace(')', '').replace(' ', '').split(',')
+    t2 = [s_.zfill(2) for s_ in t]
+    return ''.join(t2)
+
+
+def get_rsquared(date_):
+    data_path = path_root + fold_name + '\\' + date_ + '\\' + 'daily_rsquared.csv'
+    data_raw = pd.read_csv(data_path)
+    data = data_raw.iloc[2:, :]
+    data.columns = ['ymd', 'mse', 'msr', 'rsquared']
+    data['ymd'] = data['ymd'].apply(_str2datestr_)
+    data.set_index('ymd', inplace=True)
+    return data
+
+
+def get_err(date_):
+    data_path = path_root + fold_name + '\\' + date_ + '\\' + 'err_description.csv'
+    data_raw = pd.read_csv(data_path, header=None, index_col=0)
+    # data = pd.Series(data_raw)
+    return data_raw[1]
+
+
+rsquared_daily_list = list(map(get_rsquared, dates))
+rsquared_daily = pd.concat(rsquared_daily_list).sort_index()
+
+err_daily_list = list(map(get_err, dates))
+err_daily = pd.concat(err_daily_list, axis=1, keys=predict_date).T
+
+rsquared_daily.to_csv(path_root + fold_name + '\\' + 'rsquared_daily.csv')
+err_daily.to_csv(path_root + fold_name + '\\' + 'err_daily.csv')
